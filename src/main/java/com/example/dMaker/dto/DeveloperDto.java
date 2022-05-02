9fbaa83fd33f0ac38ff1d246dfcf3f61eae2cca2
kimseokjin <kimseokjin0324@gmail.com>
2022-05-02 23:38:29 +0900

    Developer 엔티티와 DeveloperDto를 분리

---
+++ src/main/java/com/example/dMaker/dto/DeveloperDto.java
@@ -0,0 +1,26 @@
+package com.example.dMaker.dto;
+
+import com.example.dMaker.entity.Developer;
+import com.example.dMaker.type.DeveloperLevel;
+import com.example.dMaker.type.DeveloperSkillType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeveloperDto {
+    private DeveloperLevel developerLevel;
+    private DeveloperSkillType developerSkillType;
+    private String memberId;
+
+    public static DeveloperDto fromEntity(Developer developer) {
+        return DeveloperDto
+                .builder()
+                .developerLevel(developer.getDeveloperLevel())
+                .developerSkillType(developer.getDeveloperSkillType())
+                .memberId(developer.getMemberId())
+                .build();
+    }
+}
